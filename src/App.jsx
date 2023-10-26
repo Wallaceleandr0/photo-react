c23d06bee6b7a160e8eab35f621a5417353bbd9e
Wallace Leandro <wallaceleandro2003@gmail.com>
2023-10-26 17:10:27 -0300

    Scroll up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import './App.css'
 import Menu from "./Components/Header/Menu"
 import { BrowserRouter as Router } from 'react-router-dom'
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const handleScrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', handleScrollToTop);
+    });
+
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', handleScrollToTop);
+      });
+    };
+  }, []);
 
   return (
     <>
